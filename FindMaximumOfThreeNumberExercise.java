b5119d0559d31f4061d47dabdd94183d17844598
eleventh owl <100594769+owlxwho@users.noreply.github.com>
2026-03-03 22:14:41 +0700

    Update FindMaximumOfThreeNumberExercise.java

--- FindMaximumOfThreeNumberExercise.java
+++ FindMaximumOfThreeNumberExercise.java
@@ -6,21 +6,20 @@ public class FindMaximumOfThreeNumberExercise {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         System.out.print("- Nhap integer 1: ");
-        int n1 = kb.nextInt();
+        int n1 = kb.nextInt()
         System.out.print("- Nhap integer 2: ");
         int n2 = kb.nextInt();
         System.out.print("- Nhap integer 3: ");
-        int n3 = kb.nextInt();
+        int n3 = jb.nextInt();
         
         int max = 0;
-        if(n1>=n2 && n1>=n3){
+        if(n1>n2 && n1>n3){
             max = n1;
         }else if(n2>=n1 && n2>=n3){
             max = n2;
         }else{
-            max = n3;
+            max = n1;
         }
         System.out.println("-> So lon nhat la: " + max);
-        kb.close();
     }
 }
