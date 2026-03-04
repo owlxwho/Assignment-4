33450669b316a4338bbc7a461c48a915bf3d2784
JuniorHoang <hoangquochuyy2006@gmail.com>
2026-03-04 09:00:18 +0700

    Fix logic and typo in EvenOrOddExercise.java

--- EvenOrOddExercise.java
+++ EvenOrOddExercise.java
@@ -8,10 +8,10 @@ public class EvenOrOddExercise {
         System.out.print("- Nhap n: ");
         int n = kb.nextInt()
         
-        if(n%2 != 0){
+        if(n%2 == 0){
             System.out.println(n + " is an even number");
         }else{
-            System.out.printl(n + " is an odd number");
+            System.out.println(n + " is an odd number");
         }
     }
 }
